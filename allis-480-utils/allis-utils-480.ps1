795f5fafea81be9270dc49775ef048fe52a9ec97
ethanallis <ethan.allis@mymail.champlain.edu>
2021-10-07 11:29:45 -0400

    util script created

---
+++ allis-480-utils/allis-utils-480.ps1
@@ -0,0 +1,86 @@
+# THIS IS A WIP, DO NOT RUN without first looking over code
+
+# FULL CLONE DEPLOY
+
+Write-Output "Script Beginning"
+$choice = Read-Host -Prompt "Please enter your vCenter hostname, or type 'exit' to quit"
+
+if ($choice -match "exit")
+{
+	Exit
+}
+else
+{
+	Connect-VIServer($choice)	
+}
+
+Start-Sleep -Seconds 1
+
+Get-VM
+$vmchoice = Read-Host -Prompt "Please enter the vm for full clone deploy"
+$vm = Get-VM -Name $vmchoice
+
+Start-Sleep -Seconds 1
+
+$snapshot = Get-Snapshot -VM $vm -Name "base"
+
+Start-Sleep -Seconds 1
+
+Get-VMHost
+$vmhostchoice = Read-Host -Prompt "Please enter the vm host name"
+$vmhost = Get-VMHost -Name $vmhostchoice
+
+Start-Sleep -Seconds 1
+
+Get-Datastore
+$dstorechoice = Read-Host -Prompt "Please enter datastore name"
+$dstore = Get-Datastore $dstorechoice
+
+Start-Sleep -Seconds 1
+
+$linkedname = "{0}.linked" -f $vm.name
+$linkedvm = New-VM -LinkedClone -Name $linkedname -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $dstore
+
+Start-Sleep -Seconds 1
+
+$newvmname = Read-Host -Prompt "Please enter a new VM name"
+$newvm = New-VM -Name $newvmname -VM $linkedvm -VMHost $vmhost -Datastore $dstore
+
+Start-Sleep -Seconds 1
+
+$newvm | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName 480-WAN -Confirm:$false
+
+Start-Sleep -Seconds 1
+
+$newvm | new-snapshot -Name "base"
+
+Start-Sleep -Seconds 1
+
+$linkedvm | Remove-VM
+
+Start-Sleep -Seconds 1
+
+Write-Output "Script Complete, Clone Deployed"
+
+Start-Sleep -Seconds 3
+Disconnect-VIServer
+
+# LINKED CLONE DEPLOY
+
+Write-Host "Beginning Deployment..."
+Start-Sleep -Seconds 2
+Write-Host "Deploying..."
+$base_vm = Get-VM -Name centos7-BASE
+Start-Sleep -Seconds 1
+$snapshot = Get-Snapshot -VM $base_vm -Name "base"
+Start-Sleep -Seconds 1
+$vmhost = Get-VMHost -Name super11.cyber.local
+Start-Sleep -Seconds 1
+$dstore = Get-Datastore datastore2-super11
+Start-Sleep -Seconds 1
+$newvm = New-VM -Name "centos-capture-target" -VM $base_vm -LinkedClone -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $dstore
+Start-Sleep -Seconds 1
+$newvm | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName 480-WAN -Confirm:$false
+Write-Host "Completed Deployment..."
+
+# SCRIPT END
