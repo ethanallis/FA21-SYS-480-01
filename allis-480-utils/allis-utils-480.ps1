8856eef99c9e9b4bccfed6263ea691ef22f25613
ethanallis <ethan.allis@mymail.champlain.edu>
2021-10-20 20:00:59 -0400

    Added Functons & Menus

--- allis-480-utils/allis-utils-480.ps1
+++ allis-480-utils/allis-utils-480.ps1
@@ -5,71 +5,89 @@ Assignment: PowerCLI & 480-Utils
 Note: This is for educational use only and should only be used as such.
 """
 
-# WORK IN PROGRESS, DO NOT RUN
-# FULL CLONE DEPLOY
+#User Menu Function
+Function UserMenu{
+    $mainuserchoice = Read-Host -Prompt "
+    `nPowerCLI Deploy Script. Options Below.
+    `n1. Full Clone Deploy
+    `n2. Linked Clone Deploy
+    `n3. Exit
+    `nPlease enter a number"
 
-Write-Output "Script Beginning"
-$choice = Read-Host -Prompt "Please enter your vCenter hostname, or type 'exit' to quit"
-
-if ($choice -match "exit")
-{
-	Exit
+    switch($mainuserchoice){
+        1{}
+        2{}
+        3{Exit}
+    }
 }
-else
-{
-	Connect-VIServer($choice)	
+
+Function VIserver{
+    $subuserchoice = Read-Host -Prompt "Please enter your vCenter hostname, or type 'exit' to quit"
+
+    if ($choice -match "exit")
+    {
+	    Exit
+    }
+    else
+    {
+	    Connect-VIServer($subuserchoice)	
+    }
 }
 
-Start-Sleep -Seconds 1
+# Full Clone Deploy Function
+Function FullClone{
+    Write-Host "Deploying full clone. User input required."
+    Start-Sleep -Seconds 1
 
-Get-VM
-$vmchoice = Read-Host -Prompt "Please enter the vm for full clone deploy"
-$vm = Get-VM -Name $vmchoice
+    Get-VM -Location "BASE-VMs"
+    $vmchoice = Read-Host -Prompt "Please enter the VM for full clone deploy"
+    $vm = Get-VM -Name $vmchoice
 
-Start-Sleep -Seconds 1
+    Start-Sleep -Seconds 1
 
-$snapshot = Get-Snapshot -VM $vm -Name "base"
+    $snapshot = Get-Snapshot -VM $vm -Name "base"
 
-Start-Sleep -Seconds 1
+    Start-Sleep -Seconds 1
 
-Get-VMHost
-$vmhostchoice = Read-Host -Prompt "Please enter the vm host name"
-$vmhost = Get-VMHost -Name $vmhostchoice
+    Get-VMHost
+    $vmhostchoice = Read-Host -Prompt "Please enter the vm host name"
+    $vmhost = Get-VMHost -Name $vmhostchoice
 
-Start-Sleep -Seconds 1
+    Start-Sleep -Seconds 1
 
-Get-Datastore
-$dstorechoice = Read-Host -Prompt "Please enter datastore name"
-$dstore = Get-Datastore $dstorechoice
+    Get-Datastore
+    $dstorechoice = Read-Host -Prompt "Please enter datastore name"
+    $dstore = Get-Datastore $dstorechoice
 
-Start-Sleep -Seconds 1
+    Start-Sleep -Seconds 1
 
-$linkedname = "{0}.linked" -f $vm.name
-$linkedvm = New-VM -LinkedClone -Name $linkedname -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $dstore
+    $linkedname = "{0}.linked" -f $vm.name
+    $linkedvm = New-VM -LinkedClone -Name $linkedname -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $dstore
 
-Start-Sleep -Seconds 1
+    Start-Sleep -Seconds 1
 
-$newvmname = Read-Host -Prompt "Please enter a new VM name"
-$newvm = New-VM -Name $newvmname -VM $linkedvm -VMHost $vmhost -Datastore $dstore
+    $newvmname = Read-Host -Prompt "Please enter a new VM name"
+    $newvm = New-VM -Name $newvmname -VM $linkedvm -VMHost $vmhost -Datastore $dstore
 
-Start-Sleep -Seconds 1
+    Start-Sleep -Seconds 1
 
-$newvm | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName 480-WAN -Confirm:$false
+    $newvm | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName 480-WAN -Confirm:$false
 
-Start-Sleep -Seconds 1
+    Start-Sleep -Seconds 1
 
-$newvm | new-snapshot -Name "base"
+    $newvm | new-snapshot -Name "base"
 
-Start-Sleep -Seconds 1
+    Start-Sleep -Seconds 1
 
-$linkedvm | Remove-VM
+    $linkedvm | Remove-VM
 
-Start-Sleep -Seconds 1
+    Start-Sleep -Seconds 1
 
-Write-Output "Script Complete, Clone Deployed"
+    Write-Output "Script Complete, Clone Deployed"
 
-Start-Sleep -Seconds 3
-Disconnect-VIServer
+    Start-Sleep -Seconds 3
+    Disconnect-VIServer
+}
 
 # LINKED CLONE DEPLOY
 
